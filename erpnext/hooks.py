d0b3f0c58181805ea7ee6c033afbe187e3f8d85e
samkun5570 <42630323+samkun5570@users.noreply.github.com>
2019-11-11 13:51:33 +0530

    Update hooks.py

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -9,7 +9,7 @@ app_icon = "fa fa-th"
 app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
-source_link = "https://github.com/frappe/erpnext"
+source_link = "https://github.com/samkun/frappe/erpnext"
 app_logo_url = '/assets/erpnext/images/erp-icon.svg'
 
 
