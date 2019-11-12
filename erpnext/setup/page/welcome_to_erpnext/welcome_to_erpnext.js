33ead5fde6fc5139b3965ccd623456b6c4817e4b
samkun5570 <42630323+samkun5570@users.noreply.github.com>
2019-11-12 11:11:05 +0530

    Update welcome_to_erpnext.js

--- erpnext/setup/page/welcome_to_erpnext/welcome_to_erpnext.js
+++ erpnext/setup/page/welcome_to_erpnext/welcome_to_erpnext.js
@@ -6,7 +6,7 @@ frappe.pages['welcome-to-erpnext'].on_page_load = function(wrapper) {
 	parent.find(".video-placeholder").on("click", function() {
 		window.erpnext_welcome_video_started = true;
 		parent.find(".video-placeholder").addClass("hidden");
-		parent.find(".embed-responsive").append('<iframe class="embed-responsive-item video-playlist" src="https://www.youtube.com/embed/videoseries?list=PL3lFfCEoMxvxDHtYyQFJeUYkWzQpXwFM9&color=white&autoplay=1&enablejsapi=1" allowfullscreen></iframe>')
+// 		parent.find(".embed-responsive").append('<iframe class="embed-responsive-item video-playlist" src="https://www.youtube.com/embed/videoseries?list=PL3lFfCEoMxvxDHtYyQFJeUYkWzQpXwFM9&color=white&autoplay=1&enablejsapi=1" allowfullscreen></iframe>')
 	});
 
 	// pause video on page change
